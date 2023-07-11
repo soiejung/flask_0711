83007be9db1ca355c95c7da7aebff2bf07cdb8b2
정소이 <soiejung@naver.com>
2023-07-11 11:27:38 +0900

    my views

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     # 따로 set FLASK_APP=파일명 적어주지 않으면 app.py를 기본 실행 입구로 찾습니다.
     app = Flask(__name__)
 
-    from .views import basic_views
+    from .views import basic_views, soie_views
     app.register_blueprint(basic_views.fisa)
-    
+    app.register_blueprint(basic_views.soie)
     return app
--- app/views/soie_views.py
+++ app/views/soie_views.py
@@ -1,21 +1,5 @@
 from flask import Blueprint
 
-                # 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
-fisa = Blueprint('basic', __name__, url_prefix='/fisa')
-
-@fisa.route('/about_me')
-def about_me():
-    return f"저는 {__name__}입니다."
-
-@fisa.route('/hello')
-def hello():
-    return "안녕하세요"
-
-@fisa.route('/bye')
-def bye():
-    return "잘가세요"
-
-
 # basic_views.soie 
                 # 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
 soie = Blueprint('soie', __name__, url_prefix='/soie')
